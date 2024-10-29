d60bb1c3efedf1d033b9a252f25a1c203a4aee4a
Erik Norder <erikpnorder@hotmail.com>
2024-10-29 14:01:39 -0300

    Implementação na camada de domínio

---
+++ src/main/java/me/dio/domain/model/User.java
@@ -0,0 +1,75 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity(name = "tb_user") //isso aqui é o nome da tabela, o que faz isso ser tornar uma tabela
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL) //quando um usuario for deletado a conta vai junto
+    private Account account;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Card card;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Feature> features;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)// toda vez que vou trazer um usuario eu quero a lista de features
+    private List<News> news;
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+}
---
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package me.dio.domain.repository;
+
+import me.dio.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> { // Usa generics para passar qual Entidade vamos usar, e o tipo do id que no caso é Long
+}
